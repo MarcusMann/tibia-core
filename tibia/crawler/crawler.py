56fd3d48ebcf1fcdc5a5fbb241f0812b5a516db1
Marcus Pereira <mpereirassa@gmail.com>
2020-05-07 03:18:33 -0300

    import parser and use

--- tibia/crawler/crawler.py
+++ tibia/crawler/crawler.py
@@ -1,8 +1,13 @@
+from .parser import Parser
+
+
 class Crawler:
     def __init__(self, downloader, default_url: str):
         self.downloader = downloader
         self.default_url = default_url
+        self.parser = Parser()
 
     async def download(self):
         response = await self.downloader.get(self.default_url)
-        return response
+
+        return  self.parser.parse(response.text)
