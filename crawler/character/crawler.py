dae5ba30e2189ac30bc1f318421d37703c01fd7c
Marcus Pereira <mpereirassa@gmail.com>
2020-05-07 20:28:25 -0300

    create build_request method

--- crawler/character/crawler.py
+++ crawler/character/crawler.py
@@ -2,11 +2,16 @@ from .parser import Parser
 
 
 class Crawler:
-    def __init__(self, downloader, default_url: str):
-        self.downloader = downloader
+    def __init__(self, downloader, default_url: str, **kwargs):
+        self.http = downloader
         self.default_url = default_url
         self.parser = Parser()
+        self.kwargs = kwargs
 
     async def download(self):
-        response = await self.downloader.get(self.default_url)
+        response = await self.build_request(self.default_url)
         return self.parser.parse(response.text)
+
+    async def build_request(self, default_url):
+        response = await self.http.post(default_url, data={"name": self.kwargs["name"]})
+        return response
