10f2f6a7084f11feb6aeb4aeb2dee1d16fbde21c
Marcus Pereira <mpereirassa@gmail.com>
2020-05-07 01:32:01 -0300

    .feat: create downloader class to requests from http protocol

---
+++ tibia/downloader.py
@@ -0,0 +1,31 @@
+from collections import namedtuple
+
+import httpx
+
+
+Response = namedtuple("Response", "text content status_code headers")
+
+
+class Downloader:
+    def __init__(self):
+        self.downloader = httpx.AsyncClient()
+
+    async def get(self, url: str):
+        response = await self.downloader.get(url)
+        
+        return Response(
+            text=response.text,
+            content=response.content,
+            status_code=response.status_code,
+            headers=response.headers
+        )
+
+    async def post(self, url):
+        response = await self.downloader.post(url)
+        
+        return Response(
+            text=response.text,
+            content=response.content,
+            status_code=response.status_code,
+            headers=response.headers
+        )
