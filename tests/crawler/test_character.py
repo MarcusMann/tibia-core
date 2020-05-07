85d3ead3a78239bc05ba064ba830a572d2ad23d6
Marcus Pereira <mpereirassa@gmail.com>
2020-05-07 20:29:11 -0300

    add name param

--- tests/crawler/test_character.py
+++ tests/crawler/test_character.py
@@ -9,6 +9,6 @@ async def test_character_crawler(mock_downloader, data_regression, output_html):
     downloader = mock_downloader.return_value
     downloader.post.return_value = mock.AsyncMock(text=output_html)
 
-    crawler = Crawler("community/?subtopic=characters", downloader)
+    crawler = Crawler("community/?subtopic=characters", downloader, name="hello")
     response = await crawler.init()
     data_regression.check(response)
